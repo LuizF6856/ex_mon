cb9f18476f39b3555caae952511ff89826ef2359
LuizF6856 <luizfellipe6843@gmail.com>
2025-06-16 21:41:35 -0300

    improve player module test

--- test/player_test.exs
+++ test/player_test.exs
@@ -1,15 +1,15 @@
-defmodule PlayerTest do
+defmodule ExMon.PlayerTest do
   use ExUnit.Case
 
   alias ExMon.Player
 
   test "build a new player" do
-    assert Player.build_player("felipe", "moderate_attack", "variate_attack", "heal") == %Player{
-             life: 100,
-             name: "felipe",
-             move_avg: "variate_attack",
-             move_rnd: "moderate_attack",
-             move_heal: "heal"
-           }
+    player = Player.build_player("felipe", "chute", "soco", "cura")
+
+    assert 100 == player.life
+    assert "felipe" == player.name
+    assert "chute" == player.moves.move_rnd
+    assert "soco" == player.moves.move_avg
+    assert "cura" == player.moves.move_heal
   end
 end
